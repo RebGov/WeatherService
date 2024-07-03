8b82ea976d91a4da4b0b2897af713d92f20eb711
Rebecca Govert <becci.govert@gmail.com>
2024-07-03 10:32:18 -0500

    remove wsl lint. update main.go code

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	cmd "weathersvc/app/cmd"
+)
 
 func main() {
-	fmt.Println("Hello World")
-
+	// signal notifycontext allows for capturing shut down/service stop signals to allow graceful stopping of service
+	ctx, stop := signal.NotifyContext(context.Background(), []os.Signal{os.Interrupt, syscall.SIGTERM, syscall.SIGINT}...)
+	defer stop()
+	if err := cmd.Execute(ctx); err != nil {
+		log.Printf("Failed to start server, err: %v", err)
+	}
 }
